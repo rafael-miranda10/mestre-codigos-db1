7669e38096ad42ddd431af20e2c0010a92fbfc17
Rafael Arthur Rocha Miranda <43150739+rafael-miranda10@users.noreply.github.com>
2020-06-24 19:39:45 -0300

    [DEV] ajuste nomenclatura

--- correcao-banca-mdc/UtilizandoPOO/2_RepresentarPessoa/Pessoa.cs
+++ correcao-banca-mdc/UtilizandoPOO/2_RepresentarPessoa/Pessoa.cs
@@ -9,37 +9,37 @@ namespace UtilizandoPOO._2_RepresentarPessoa
         {
         }
 
-        private string _Nome;
-        private double _Altura;
-        private DateTime _DaTaNascimento;
+        private string _nome;
+        private double _altura;
+        private DateTime _daTaNascimento;
 
         public string Nome
         {
-            get => _Nome;
-            set => _Nome = value;
+            get => _nome;
+            set => _nome = value;
         }
 
         public double Altura
         {
-            get => _Altura;
-            set => _Altura = value;
+            get => _altura;
+            set => _altura = value;
         }
 
         public DateTime DaTaNascimento
         {
-            get => _DaTaNascimento;
-            set => _DaTaNascimento = value;
+            get => _daTaNascimento;
+            set => _daTaNascimento = value;
         }
 
         public int CalcularIdade()
         {
-            return DateTime.Now.Year - _DaTaNascimento.Year - (_DaTaNascimento.DayOfYear < DateTime.Now.DayOfYear ? 0 : 1);
+            return DateTime.Now.Year - _daTaNascimento.Year - (_daTaNascimento.DayOfYear < DateTime.Now.DayOfYear ? 0 : 1);
         }
 
         public override string ToString()
         {
-            return string.Format(ConstantesPoo.ExibirDadosPessoa, _Nome, _Altura,
-                                _DaTaNascimento.ToShortDateString(), CalcularIdade());
+            return string.Format(ConstantesPoo.ExibirDadosPessoa, _nome, _altura,
+                                _daTaNascimento.ToShortDateString(), CalcularIdade());
         }
     }
 }
