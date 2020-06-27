15e27347be890fea4e58caa20befa4d09f856db8
Rafael Arthur Rocha Miranda <43150739+rafael-miranda10@users.noreply.github.com>
2020-06-27 15:06:35 -0300

    [DEV] fabrica bhaskara - console

---
+++ correcao-banca-mdc/TrabalhandoNoConsole/5_Bhaskara/FabricaBhaskara.cs
@@ -0,0 +1,10 @@
+﻿namespace TrabalhandoNoConsole._5_Bhaskara
+{
+    public class FabricaBhaskara
+    {
+        public static Bhaskara Criar(double a, double b, double c)
+        {
+            return new Bhaskara(a, b, c);
+        }
+    }
+}
